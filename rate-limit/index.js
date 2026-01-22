39d8c12a921db5813e790e1275a49e603cfa7e88
redaelemawy <redaelemawy98@gmail.com>
2026-01-23 01:12:28 +0200

    apply rate limit on group of routes

--- rate-limit/index.js
+++ rate-limit/index.js
@@ -9,13 +9,23 @@ const rateLimiter = expressRateLimit({
   message: 'Too many requests, try after 10 second',
 });
 // app.use(rateLimiter); // apply it globally
+app.use('/api/auth', rateLimiter); // apply only on route group
 
 app.get('/api', (req, res) => {
   res.send('hello');
 });
-app.get('/api/secure/', rateLimiter, (req, res) => {
-  res.send('this is secure route');
+
+app.get('/api/auth/login', (req, res) => {
+  res.send('get here to login');
 });
+
+app.get('/api/auth/me', (req, res) => {
+  res.send('you are reda');
+});
+// only apply on given route
+// app.get('/api/secure/', rateLimiter, (req, res) => {
+//   res.send('this is secure route');
+// });
 
 app.listen(3000, () => {
   console.log('app rate limit is running ');
